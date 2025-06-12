518a6335d2252f9ed8faa4b82979c6934b5f1048
Shadow091101 <manavnaik442@gmail.com>
2025-06-13 03:22:56 +0530

    Commit changes 13/6/25 03:22

--- Personal Expense Tracker/main.py
+++ Personal Expense Tracker/main.py
@@ -70,7 +70,7 @@ class App(Tk):
         b3=Button(mainFrame,text="Export CSV",font=(20))
         b3.pack(side="left", expand=True,pady=10,padx=20)
         self.f1=LabelFrame(self,text="",font=(30))
-        self.f1.pack(ipadx=10,ipady=20)    
+        self.f1.pack(ipadx=20,ipady=20,padx=30,pady=30)    
         self.AddExpense()        
         
     def date_and_time(self):
@@ -84,6 +84,9 @@ class App(Tk):
         return f"{curr_year}-{curr_month}-{curr_date} {curr_hour}:{curr_minute}:{curr_second}"        
     
     def AddExpense(self):
+        self.categories=["Food","Beverages","Emergency","Study","Medical Expenses","Entertainment","Travel","Clothing","Utilities/Bills","Lend","Gifts","Family","Friends","Others"]
+        self.opt=StringVar()
+        self.opt.set(value="Others")
         self.datetime=StringVar()    
         self.datetime.set(self.date_and_time())
         for widget in self.f1.winfo_children():
@@ -96,7 +99,7 @@ class App(Tk):
         
         self.l2=Label(self.f1,text="Category    ",font=("Arial",15))
         self.l2.grid(row=2,column=1,sticky="w")
-        self.e2=Entry(self.f1,width=80)
+        self.e2=OptionMenu(self.f1,self.opt,*self.categories)
         self.e2.grid(row=2,column=2,sticky="w")
         
         self.l3=Label(self.f1,text="Date         ",font=("Arial",15))
@@ -120,7 +123,7 @@ class App(Tk):
     def reset_records(self):
         if messagebox.askyesno("Reset","Do you want to reset the entries? "):
             self.e1.delete(0,END)
-            self.e2.delete(0,END)
+            self.opt.set(value="Others")
             self.datetime.set(self.date_and_time())
             self.tf1.delete("1.0","end")
     
@@ -133,7 +136,7 @@ class App(Tk):
                         """
             values = (
                 self.e1.get(),
-                self.e2.get(),
+                self.opt.get(),
                 self.e3.get(),
                 self.tf1.get("1.0", "end-1c")
             )
@@ -142,7 +145,7 @@ class App(Tk):
                 self.cursor.execute(submit_query,values)
                 messagebox.showinfo("Saved", "Your expense has been saved to the database!")
                 self.e1.delete(0,END)
-                self.e2.delete(0,END)
+                self.opt.set(value="Others")
                 self.datetime.set(self.date_and_time())
                 self.tf1.delete("1.0","end")
                 self.connection.commit()
@@ -247,7 +250,10 @@ class App(Tk):
         self.AddExpense() 
         self.init_db()  
         
-    def View_Expense(self):
+    select_query_="SELECT amount,category,description,datetime FROM expenses ORDER BY datetime DESC ;"
+    def View_Expense(self,query=None):
+        if query==None:
+            query=self.select_query_
         for widget in self.f1.winfo_children():
             widget.destroy()
         self.f1.config(text="View Expense")
@@ -274,11 +280,13 @@ class App(Tk):
         self.f1.grid_rowconfigure(0, weight=1)
         self.f1.grid_columnconfigure(0, weight=1)
         try:
-            select_query="SELECT amount,category,description,datetime FROM expenses ORDER BY datetime DESC ;"
+            select_query=query
             self.cursor.execute(select_query)
             result=self.cursor.fetchall()
             if not result:
                 Label(self.scroll_frame,text="No records found").grid(row=0,column=0,padx=10,pady=10)
+                button1=Button(self.f1,text="Filter",command=filter_window)
+                button1.grid(row=2,column=1,sticky="ew",padx=10,pady=10)
                 return
             
             column_width={
@@ -294,7 +302,6 @@ class App(Tk):
                 self.col_label=Label(self.scroll_frame,text=name,font=("Arial",10,"bold"), borderwidth=1, relief="solid", width=self.width)
                 self.col_label.grid(row=0,column=col,sticky="nsew")
         
-           
             for row_num,row_data in enumerate(result,start=1):
                 for col_num,cell_data in enumerate(row_data):
                     col_name = column_names[col_num]
@@ -307,6 +314,44 @@ class App(Tk):
         except mysql.connector.Error as err:
             Label(self.scroll_frame, text=f"Database error: {err}").grid(row=0, column=0)
             print(err)
+        
+        def filter_window():
+            filter_top_level=Toplevel(self.f1)
+            filter_window_frame=Frame(filter_top_level)
+            filter_window_frame.pack(padx=10,pady=10,ipadx=10,ipady=10)
+            filter_window_label_1=Label(filter_window_frame,text="Select What you want to sort : ")
+            filter_window_label_1.grid(row=0,column=0,columnspan=2)
+            
+            amount_filter_var=StringVar()
+            amount_filter_var.set("")
+            amount_filter_options=["BETWEEN 0 AND 10","BETWEEN 10 AND 50","BETWEEN 50 AND 100","BETWEEN 100 AND 500",">500"]
+            
+            def op(*args):
+                query=f"SELECT amount,category,description,datetime FROM expenses WHERE amount {amount_filter_var.get()} ORDER BY datetime DESC ;"
+                print("Select :",query)
+                self.View_Expense(query)
+                print("Success",amount_filter_var.get())
+            
+            amount_filter_var.trace_add("write",op)
+            amount_filter_label=Label(filter_window_frame,text="Amount")
+            amount_filter_label.grid(row=1,column=0)
+            amount_option_filter=OptionMenu(filter_window_frame,amount_filter_var,*amount_filter_options)
+            amount_option_filter.grid(row=1,column=1)
+            
+            # category_filter_button1=Button(filter_window_frame,text="Category",command=filter_categroy)
+            # category_filter_button1.grid(row=1,column=0)
+            
+            
+                
+            
+            pass
+        
+        button1=Button(self.f1,text="Filter",command=filter_window)
+        button1.grid(row=2,column=1,sticky="ew",padx=10,pady=10)
+        
+            
+        
+        
         
 
 if __name__=="__main__":
